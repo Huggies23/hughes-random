f6aa8c5c8f4b1172214527c97425f16668c7ac17
Huggies23 <38754794+Huggies23@users.noreply.github.com>
2018-05-18 00:47:45 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    #Author info and package location
+    name ='hughes',
+	url = 'https://github.com/Huggies23/hughes-random',
+	author = 'David Hughes',
+	author_email = 'david.james.hughes13@gmail.com',
+	#packaging and installation parameters
+	packages = ['hughes'],
+	install_requires=['numpy', 'matplotlib.pylab'],
+	version = '0.1',
+	licence = 'N/A',
+	description = 'Produce objects containing random numbers generated from a given statistical distribution'
+	#long_desription = open('README.txt').read()
+)
